4f9b2c2a7f6617330d20ee286b7595c68f602dd0
SvetlanaLana13 <karzaevasvetlana@gmail.com>
2024-11-20 19:40:00 +0300

    Add files via upload
    module_1_4

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+#my_string=input("меня зовут светлана")
+print("меня зовут светлана".upper())
+print("меня зовут Светлана".lower())
+print("Меня Зовут Светлана".replace(" ",""))
+my_string=("Меня зовут светлана")
+print(my_string[0])
+print(my_string[18])
